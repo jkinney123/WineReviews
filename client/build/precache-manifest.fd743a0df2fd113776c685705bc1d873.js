ef16f5f38d59aeff07e6bd598bf97c90f3a913a5
Joe Kinney <joekinney53@gmail.com>
2019-12-07 19:27:43 -0600

    added gh-pges link in packagejson

---
+++ client/build/precache-manifest.fd743a0df2fd113776c685705bc1d873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "debf1adf4ba04c428906",
+    "url": "/static/js/main.debf1adf.chunk.js"
+  },
+  {
+    "revision": "cf44680e98eafc922503",
+    "url": "/static/js/2.cf44680e.chunk.js"
+  },
+  {
+    "revision": "debf1adf4ba04c428906",
+    "url": "/static/css/main.dcd5050f.chunk.css"
+  },
+  {
+    "revision": "0a3baf3890bdf52763f05098367ad21e",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df1e2832a91f4573c48b83b366560c83.js"
+  "/precache-manifest.fd743a0df2fd113776c685705bc1d873.js"
 );
 
 workbox.clientsClaim();
